ee73f8a29de7036bd47d7abd717d56cb5807e854
Jędrzej Stasik <jedrzej.stasik@gmail.com>
2024-01-11 12:24:26 +0100

    feat: WB

--- src/__main__.py
+++ src/__main__.py
@@ -2,8 +2,23 @@ from ctypes.wintypes import RGB
 from time import time
 
 import cv2
+import numpy as np
 
 WINDOW_NAME = "Main"
+
+
+def white_balance(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - (
+        (avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1
+    )
+    result[:, :, 2] = result[:, :, 2] - (
+        (avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1
+    )
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
 
 
 def empty(x):
@@ -90,6 +105,12 @@ while True:
     # Display the resulting frame
     # cv2.imshow(WINDOW_NAME, frame)
     efect = BrightnessContrast(frame, 0)
+
+    org = efect.copy()
+    cv2.imshow("orginal", org)
+
+    efect = white_balance(efect)
+
     cv2.imshow(WINDOW_NAME, efect)
 
     video_out.write(frame)
