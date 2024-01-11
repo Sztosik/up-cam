9ebeea08d00406278907e9eb652c1d9c9c56311d
Jędrzej Stasik <jedrzej.stasik@gmail.com>
2024-01-11 12:07:44 +0100

    feat: image manipulation

--- src/__main__.py
+++ src/__main__.py
@@ -3,6 +3,67 @@ from time import time
 
 import cv2
 
+WINDOW_NAME = "Main"
+
+
+def empty(x):
+    pass
+
+
+def BrightnessContrast(img, brightness=0):
+    # getTrackbarPos returns the current
+    # position of the specified trackbar.
+    brightness = cv2.getTrackbarPos("Brightness", WINDOW_NAME)
+    contrast = cv2.getTrackbarPos("Contrast", WINDOW_NAME)
+    effect = controller(img, brightness, contrast)
+    # The function imshow displays an image
+    # in the specified window
+    return effect
+
+
+def controller(img, brightness=255, contrast=127):
+    brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255))
+    contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
+
+    if brightness != 0:
+        if brightness > 0:
+            shadow = brightness
+            max = 255
+        else:
+            shadow = 0
+            max = 255 + brightness
+
+        al_pha = (max - shadow) / 255
+        ga_mma = shadow
+
+        # The function addWeighted calculates
+        # the weighted sum of two arrays
+        cal = cv2.addWeighted(img, al_pha, img, 0, ga_mma)
+    else:
+        cal = img
+
+    if contrast != 0:
+        Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast))
+        Gamma = 127 * (1 - Alpha)
+
+        # The function addWeighted calculates
+        # the weighted sum of two arrays
+        cal = cv2.addWeighted(cal, Alpha, cal, 0, Gamma)
+
+    # putText renders the specified text string in the image.
+    cv2.putText(
+        cal,
+        "B:{},C:{}".format(brightness, contrast),
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2,
+    )
+
+    return cal
+
+
 # define a video capture object
 camera = cv2.VideoCapture(0)
 
@@ -11,12 +72,22 @@ ret, frame = camera.read()
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 video_out = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
 
+cv2.namedWindow(WINDOW_NAME)
+
+# Brightness range -255 to 255
+cv2.createTrackbar("Brightness", WINDOW_NAME, 255, 2 * 255, empty)
+
+# Contrast range -127 to 127
+cv2.createTrackbar("Contrast", WINDOW_NAME, 127, 2 * 127, empty)
+
 while True:
     # Capture the video frame by frame
     ret, frame = camera.read()
 
     # Display the resulting frame
-    cv2.imshow("frame", frame)
+    # cv2.imshow(WINDOW_NAME, frame)
+    efect = BrightnessContrast(frame, 0)
+    cv2.imshow(WINDOW_NAME, efect)
 
     video_out.write(frame)
 
@@ -30,7 +101,7 @@ while True:
 
     # press 's' to capture the photo
     if key & 0xFF == ord("s"):
-        cv2.imwrite(f"img/img_{int(time())}.png", frame)
+        cv2.imwrite(f"img/img_{int(time())}.png", efect)
         print(f"saved as img_{int(time())}.png")
 
 
